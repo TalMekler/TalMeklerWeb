673efc2ae6eac307e9c28e6e9aea1a866c21acc3
tal mekler <tal.mekler11@gmail.com>
2020-09-20 20:29:10 +0300

    first page
    LANDING PAGE

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
         setTimeout(function(){
             $("header .logo .logo-text").fadeIn(1000);
             $(".first-page").addClass("active");
-        }, 500)
+            setTimeout(function(){
+                $(".first-page .main-text").addClass("active");
+            }, 900);
+        }, 500);
     })
 });
