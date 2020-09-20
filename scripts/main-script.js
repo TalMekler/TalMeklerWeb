81859fd5733b64be617e3dabd556f3cc31342de7
tal mekler <tal.mekler11@gmail.com>
2020-09-20 18:14:35 +0300

    ..
    Header + First Page

---
+++ scripts/main-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(function () {
+        setTimeout(function(){
+            $("header .logo .logo-text").fadeIn(1000);
+            $(".first-page").addClass("active");
+        }, 500)
+    })
+});
