95f6d00a8e6848e9685bcc05cc46236750eff352
tal mekler <tal.mekler11@gmail.com>
2020-09-21 13:09:17 +0300

    ..
    >> Header -> black when page scroll down
    >> Second Page -> Adding
    >> Circle -> Fix to second page

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
     $(function () {
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            if (scroll > 100){
+                $("header").addClass("page-down");
+            }else{
+                $("header").removeClass("page-down")
+            }
+        })
+        
+    })
+    $(function () {
         setTimeout(function(){
             $("header .logo .logo-text").fadeIn(1000);
-            $(".first-page").addClass("active");
+            $(".first-page, .second-page").addClass("active");
             setTimeout(function(){
                 $(".first-page .main-text").addClass("active");
             }, 700);
