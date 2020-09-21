1e92f01e644c5b767d647d94ef5174066b59e960
tal mekler <tal.mekler11@gmail.com>
2020-09-21 15:28:05 +0300

    Update main-script.js

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Header on SCROLL
     $(function () {
         var scroll = $(window).scrollTop();
             if (scroll > 100){
@@ -16,9 +17,9 @@ $(document).ready(function () {
         })
         
     })
+    // Show Elements when page LOAD
     $(function () {
         setTimeout(function(){
-            // $("header .logo .logo-text").fadeIn(1000);
             $("header").fadeIn(1000);
             $(".first-page, .second-page").addClass("active");
             setTimeout(function(){
@@ -26,4 +27,5 @@ $(document).ready(function () {
             }, 700);
         }, 500);
     })
+
 });
