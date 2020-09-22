d834fa76a393223ce38cf4b0283a5ad3aede02f4
tal mekler <tal.mekler11@gmail.com>
2020-09-22 10:38:33 +0300

    adding second page

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -2,30 +2,50 @@ $(document).ready(function () {
     // Header on SCROLL
     $(function () {
         var scroll = $(window).scrollTop();
-            if (scroll > 100){
-                $("header").addClass("page-down");
-            }else{
-                $("header").removeClass("page-down")
-            }
-        $(window).scroll(function(){
+        if (scroll > 100) {
+            $("header").addClass("page-down");
+        } else {
+            $("header").removeClass("page-down")
+        }
+        $(window).scroll(function () {
+
             var scroll = $(this).scrollTop();
-            if (scroll > 100){
+            if (scroll > 100) {
                 $("header").addClass("page-down");
-            }else{
+            } else {
                 $("header").removeClass("page-down")
             }
         })
-        
+
     })
     // Show Elements when page LOAD
     $(function () {
-        setTimeout(function(){
+        setTimeout(function () {
             $("header").fadeIn(1000);
-            $(".first-page, .second-page").addClass("active");
-            setTimeout(function(){
-                $(".first-page .main-text").addClass("active");
+            $(".first-page").addClass("active");
+            setTimeout(function () {
+                $(".first-page .main-content").addClass("active");
             }, 700);
         }, 500);
+
+        function show_section(section) {
+            var scroll = $(window).scrollTop();
+            var top = $(section).position().top - scroll - 100;
+            var window_height = $(window).height();
+            if (top < (window_height / 1.5)) {
+                $(section).addClass("active");
+                $(section).children(".main-content").children().each(function(){
+                    if(!$(this).hasClass("active")){
+                        $(this).addClass("active");
+                    }
+                });
+            }
+        }
+        show_section(".second-page");
+        $(window).scroll(function () { 
+            show_section(".second-page");
+        });
+
     })
 
 });
