d8c10ba7db76caa49bd90b38f63d821a0e09ff30
tal mekler <tal.mekler11@gmail.com>
2020-09-26 07:35:54 +0300

    smooth scroll

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -43,6 +43,15 @@ $(document).ready(function () {
         })
 
     })
+    // Smooth Scroll
+    $(function(){
+        $(".scroll-link").click(function(e){
+            e.preventDefault();
+            $("body, html").animate({
+                scrollTop: $(this.hash).offset().top
+            }, 1250)
+        })
+    })
     // Show Elements when page LOAD
     $(function () {
         setTimeout(function () {
