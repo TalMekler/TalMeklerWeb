633f863f8419516ac80ea51f0ddb69670c7462c7
tal mekler <tal.mekler11@gmail.com>
2020-09-26 07:53:55 +0300

    fix smooth scroll

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -44,11 +44,23 @@ $(document).ready(function () {
 
     })
     // Smooth Scroll
-    $(function(){
-        $(".scroll-link").click(function(e){
+    $(function () {
+        var header_height = 100;
+        if ($(window).width() < 999) {
+            header_height = 92;
+        }
+        $(".scroll-link").click(function (e) {
             e.preventDefault();
+            var header_height = 100;
+            if ($(window).width() < 999) {
+                header_height = 92;
+            }
+            var firstSecCheck = $(this.hash).attr("data-first");
+            if (firstSecCheck) {
+                header_height = 0;
+            }
             $("body, html").animate({
-                scrollTop: $(this.hash).offset().top
+                scrollTop: $(this.hash).offset().top - header_height
             }, 1250)
         })
     })
