9fe34d13414e6295b67c61c82cf0bb2c07e43f5a
tal mekler <tal.mekler11@gmail.com>
2020-10-13 12:06:57 +0300

    add FADEIN effect - Scroll Trigger

---
+++ scripts/show-elements-scrollTriger.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var sections = $("main section.trigger");
+    var section_children, show_timing = 10;
+    $(window).scroll(function () {
+        var window_scroll = $(this).scrollTop();
+        sections.each(function () {
+            section_children = $(this).children();
+            section_children_pos = $(section_children).offset().top - window_scroll;
+            if(section_children_pos <= $(window).height() - ($(window).height() / show_timing)){
+                section_children.addClass("show");
+            } else if(section_children_pos > $(window).height()){
+                section_children.removeClass("show");
+            }
+        })
+    });
+});
