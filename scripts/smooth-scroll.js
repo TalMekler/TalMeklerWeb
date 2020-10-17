a41420ac05c20548bddaf82b196807e2eab913f3
tal mekler <tal.mekler11@gmail.com>
2020-10-17 11:23:20 +0300

    Update smooth-scroll.js

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,33 +1,3 @@
-// const smooth_links = document.querySelectorAll('.scroll');
-// var interval_time = 1;
-// smooth_links.forEach((link) => {
-//     link.addEventListener('click', (link) => {
-//         console.log('hi');
-//         link.preventDefault();
-//         var href = link.target.getAttribute('href');
-//         var destination = document.querySelector(href);
-//         var end = destination.offsetTop;
-//         var current = 0;
-//         var add_to_current;
-//         var interval = setInterval(() => {
-//             if (current > 50 && current < end-50){
-//                 add_to_current = 20;
-//             }else{
-//                 add_to_current = 10;
-//             }
-//             current += add_to_current;
-//             if (current > end) {
-//                 current = end;
-//             }
-//             window.scrollTo(0, current)
-//             if (current >= end) {
-//                 clearInterval(interval);
-//             }
-//         }, interval_time);
-//     })
-// })
-
-
 function smoothScroll(target, duration) {
     var target = document.querySelector(target);
     var targetPosition = target.offsetTop;
