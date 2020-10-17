d6f3ccffbae679d17efc8a547615ffbe7e0db4db
tal mekler <tal.mekler11@gmail.com>
2020-10-17 11:24:42 +0300

    Update controll-nav.js

--- scripts/controll-nav.js
+++ scripts/controll-nav.js
@@ -4,15 +4,18 @@ const nav_bar = document.querySelector('nav');
 const nav_items = document.querySelectorAll('nav .nav-item');
 
 open_bar.addEventListener('click', ()=>{
+    nav_bar.style.transition = 'all .3s ease-in-out';
     nav_bar.classList.add('open');
     document.body.style.overflow = 'hidden';
 })
 close_bar.addEventListener('click', ()=>{
+    nav_bar.style.transition = 'all .3s ease-in-out';
     nav_bar.classList.remove('open');
     document.body.style.overflow = 'visible';
 })
 nav_items.forEach((e)=>{
     e.addEventListener('click', ()=>{
+        nav_bar.style.transition = 'none';
         nav_bar.classList.remove('open');
         document.body.style.overflow = 'visible';
     })
