5f54dcd6134f1f4e8aa7f6e7366a42e023b47e5b
tal mekler <tal.mekler11@gmail.com>
2020-10-19 10:54:27 +0300

    Update fix-hero-section.js

--- scripts/fix-hero-section.js
+++ scripts/fix-hero-section.js
@@ -2,7 +2,7 @@ const hero_section = document.querySelector('section.hero');
 function fixHeroSection() {
     var headerHeight = document.querySelector('header').offsetHeight;
     var platform_img_height = document.querySelector('section.hero img.all-platforms').offsetHeight;
-    var calc = window.innerHeight - (headerHeight + (platform_img_height/2));
+    var calc = window.innerHeight - (headerHeight + (platform_img_height * 0.2));
     if (checkMobile()) {
         hero_section.style.minHeight = calc + 'px';
     } else {
