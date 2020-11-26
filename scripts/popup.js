b75248917afff8e587c81ec72b494545ccb29bb0
tal mekler <tal.mekler11@gmail.com>
2020-11-26 13:06:14 +0200

    popup - MAKE

---
+++ scripts/popup.js
@@ -0,0 +1,14 @@
+var popup_wrapper = document.querySelector('#popup-wrapper');
+var close_popup_btn = document.querySelector('#popup-wrapper .popup-box .close-popup');
+
+window.addEventListener('load', ()=>{
+    setTimeout(() => {
+        popup_wrapper.style.display = 'flex';
+        document.querySelector('body').classList.add('popup-open');
+    }, 2500);
+
+    close_popup_btn.addEventListener('click', ()=>{
+        popup_wrapper.style.display = 'none';
+        document.querySelector('body').classList.remove('popup-open');
+    })
+})
