e1ad7dced98c9c037bb4f0f8e43811158561d86c
tal mekler <tal.mekler11@gmail.com>
2021-01-28 14:47:37 +0200

    .

--- scripts/theme-changer.js
+++ scripts/theme-changer.js
@@ -1,5 +1,27 @@
+setFirstTheme();
 // ### Theme changer by click ###
 const toggle_theme_btn = document.querySelector("header nav ul .toggle-theme-wrapper .toggle-theme");
 toggle_theme_btn.addEventListener("click", () => {
     document.querySelector("body").classList.toggle("light-theme");
+    saveTheme();
 })
+
+function saveTheme(){
+    if (document.querySelector("body").classList.contains("light-theme")){
+        localStorage.setItem("theme", "light-theme");
+    }else{
+        localStorage.setItem("theme", "dark-theme");
+    }
+}
+
+if (localStorage.getItem("theme") == "light-theme") {
+    document.querySelector("body").classList.add("light-theme");
+}else {
+    document.querySelector("body").classList.remove("light-theme");
+}
+
+function setFirstTheme(){
+    if (localStorage.getItem("theme") == null) {
+        localStorage.setItem("theme", "dark-theme");
+    }
+}
